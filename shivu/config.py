5dd2b399d3284fd1fe1bc390be1505fed196df89
VILLAIN <iamrkay420@gmail.com>
2024-12-19 21:17:29 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,8 +5,8 @@ class Config(object):
     OWNER_ID = "5909658683"
     sudo_users = "5909658683", "8019277081", "5608779258", "6961368696", "1881562083", "8035449599", "7430528632"
     GROUP_ID = -1002311769574
-    TOKEN = "76553519"
-    mongo_url = ""
+    TOKEN = "7655351916:AAH_Sen1msMhGk8lNHyQTC1RWzMJfbjV77g"
+    mongo_url = "mongodb+srv://Villainmusic01:deathnote0p@cluster0.nah8e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://files.catbox.moe/wy70cl.jpg", "https://files.catbox.moe/wy70cl.jpg"]
     SUPPORT_CHAT = "WH_SUPPORT_GC"
     UPDATE_CHAT = "iamvillain77"
