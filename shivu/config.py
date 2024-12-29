5da9481082f4ead268d22fbc364aaf5bc2febb06
VILLAIN <iamrkay420@gmail.com>
2024-12-29 16:50:48 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,10 +3,10 @@ class Config(object):
 
     # Get this value from my.telegram.org/apps
     OWNER_ID = "5909658683"
-    sudo_users = "5909658683", "8019277081", "5608779258", "6961368696", "1881562083", "8035449599", "7430528632"
+    sudo_users = "5909658683", "8019277081", "5608779258", "6961368696", "1881562083", "8035449599", "7430528632", "7590975069"
     GROUP_ID = -1002311769574
-    TOKEN = ""
-    mongo_url = ""
+    TOKEN = "7655351916:AAH_Sen1msMhGk8lNHyQTC1RWzMJfbjV77g"
+    mongo_url = "mongodb+srv://akshitmusic59:deathnote0p@cluster0.n1mam.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://files.catbox.moe/wy70cl.jpg", "https://files.catbox.moe/wy70cl.jpg"]
     SUPPORT_CHAT = "WH_SUPPORT_GC"
     UPDATE_CHAT = "iamvillain77"
